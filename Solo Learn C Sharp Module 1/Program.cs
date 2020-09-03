58c97769df7e3251d55f546db6f052b7983f0bfd
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-09-03 12:31:17 -0400

    Commited

--- Solo Learn C Sharp Module 1/Program.cs
+++ Solo Learn C Sharp Module 1/Program.cs
@@ -10,6 +10,11 @@ namespace Solo_Learn_C_Sharp_Module_1
             int myAge = 16;
             Console.WriteLine(myAge);
 
+            // Your First C# Program 
+            // You can run, save, and share your C# codes on our Code Playground, without installing any additional software.
+
+
+
 
 
 
