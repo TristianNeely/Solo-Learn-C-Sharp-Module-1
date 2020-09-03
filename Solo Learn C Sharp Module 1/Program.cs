2f04175eac1c56ecafafd63e0cf83c15a4006d3f
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-09-03 12:33:50 -0400

    Printing Text

--- Solo Learn C Sharp Module 1/Program.cs
+++ Solo Learn C Sharp Module 1/Program.cs
@@ -6,12 +6,17 @@ namespace Solo_Learn_C_Sharp_Module_1
     {
         static void Main(string[] args)
         {
+           
+
             // Variable Types
             int myAge = 16;
             Console.WriteLine(myAge);
 
             // Your First C# Program 
             // You can run, save, and share your C# codes on our Code Playground, without installing any additional software.
+
+            // Priniting Text
+            Console.WriteLine("bruh");
 
 
 
