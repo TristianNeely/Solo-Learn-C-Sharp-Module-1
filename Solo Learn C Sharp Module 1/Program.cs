3b8361a4c1ccd8cae66d2f3dd0e87fa7b7eb09d0
Neely_Tristian <Neely_Tristian@edu.mcctc.net>
2020-09-03 12:29:26 -0400

    Add project files.

---
+++ Solo Learn C Sharp Module 1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Solo_Learn_C_Sharp_Module_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Types
+            int myAge = 16;
+            Console.WriteLine(myAge);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
